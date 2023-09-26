df3dcf6113e698f14cc4bfcc7c46150c4ead5690
Batuhan Göbekli <batuhangobekli@gmail.com>
2023-09-26 16:46:36 +0300

    Initial commit for quiz app

---
+++ flutter_complete_guide/lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'question.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+  var titles = ["First Question", "Second Question", "Third Question"];
+
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex += 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My App"),
+        ),
+        body: Column(
+          children: [
+            Question(titles[_questionIndex]),
+            ElevatedButton(onPressed: _answerQuestion, child: Text('Answer 1')),
+            ElevatedButton(onPressed: _answerQuestion, child: Text('Answer 2')),
+            ElevatedButton(onPressed: _answerQuestion, child: Text('Answer 3')),
+          ],
+        ),
+      ),
+    );
+  }
+}
